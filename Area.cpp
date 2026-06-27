c833069575a253393809550c23d92d67d90d6086
krushnashinde534-dotcom <krushnashinde534@gmail.com>
2026-06-27 10:30:49 +0530

    Add program to calculate area of a square

---
+++ Area.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int side, area;
+
+    cout << "Enter the side of the square: ";
+    cin >> side;
+
+    area = side * side;
+
+    cout << "Area of the square = " << area << endl;
+
+    return 0;
+}
