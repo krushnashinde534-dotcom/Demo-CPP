73e67acf5104bc24897853fb644ea021279bebad
krushna shinde <krushnashinde534@gmail.com>
2026-07-08 19:49:49 +0530

    Student Info using OOP

---
+++ OOP/Student.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s;
+
+    s.name = "Krushna";
+    s.age = 19;
+
+    s.display();
+
+    return 0;
+}
