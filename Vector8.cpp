46008b533f8d069c5e6e77d9e4e3f873fbbc5a1a
krushna shinde <krushnashinde534@gmail.com>
2026-07-10 19:26:15 +0530

    Reverse a Vector

---
+++ Vector8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    reverse(v.begin(), v.end());
+
+    for (int x : v) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
