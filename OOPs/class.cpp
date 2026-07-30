526bf45d6eed37b466a8ba9bbf2aa9c152d3e98a
krushna shinde <krushnashinde534@gmail.com>
2026-07-30 20:09:37 +0530

    Data types

---
+++ OOPs/class.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float marks = 85.5;
+    double salary = 25000.75;
+    char grade = 'A';
+    bool passed = true;
+    string name = "Rahul";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Passed: " << passed << endl;
+
+    return 0;
+}
