d7704ebb2135d934ae860c505c60cefa9042ab58
krushna shinde <krushnashinde534@gmail.com>
2026-08-06 18:27:55 +0530

    Find lenght of string

---
+++ String/str3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "Length = " << str.length();
+
+    return 0;
+}
